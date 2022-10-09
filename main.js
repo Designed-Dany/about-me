eca366976fb6fdb8fcf44635ab70a6354d7cea90
Dany <githubs69@gmail.com>
2022-10-09 23:53:19 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+let offset = 0; // смещение от левого края;
+const sliderLine = document.querySelector('.slider-line');
+let container = document.querySelector('.container');
+let alertError = document.querySelector('.error');
+
+document.querySelector('.arrow-left-btn').addEventListener('click', function() {
+    offset = offset + 750;
+    if (offset > 2000) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+})
+
+document.querySelector('.arrow-right-btn').addEventListener('click', function() {
+    offset = offset - 750;
+    if (offset < 0) {
+        offset = 1500;
+    }
+    sliderLine.style.left = -offset + 'px';
+})
+
+alertError.addEventListener('click', function () {
+    alert('Ошибка - это повод хорошо подумать:)')
+})
+
+document.addEventListener('DOMContentLoaded', hiddenCloseclick());
+document.getElementById('click-to-hide').addEventListener('click', hiddenCloseclick);
+
+function hiddenCloseclick() {
+	let x = document.getElementById('hidden-element');
+      if (x.style.display == "none"){
+	  x.style.display = "block";
+	  } else {
+	 x.style.display = "none"}
+    };
