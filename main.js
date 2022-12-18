6fdc014150bf84c185e4328e62f55bafc92711ef
Designed-Dany <githubs69@gmail.com>
2022-12-18 14:25:55 +0500

    new project

--- main.js
+++ main.js
@@ -1,40 +1,53 @@
-let offset = 0; // смещение от левого края;
-const sliderLine = document.querySelector('.slider-line');
-let container = document.querySelector('.container');
-let alertError = document.querySelector('.error');
-let inputEmail = document.querySelector('.input-email');
-let formInputs = document.querySelectorAll('.js-input');
-document.querySelector('.arrow-left-btn').addEventListener('click', function() {
-    offset = offset + 750;
-    if (offset > 2000) {
-        offset = 0;
-    }
-    sliderLine.style.left = -offset + 'px';
-})
 
-document.querySelector('.arrow-right-btn').addEventListener('click', function() {
-    offset = offset - 750;
-    if (offset < 0) {
-        offset = 1500;
-    }
-    sliderLine.style.left = -offset + 'px';
-})
+    let offset = 0; // смещение от левого края;
+    const sliderLine = document.querySelector('.slider-line');
+    let container = document.querySelector('.container');
+    let alertError = document.querySelector('.error');
+    let inputEmail = document.querySelector('.input-email');
+    let formInputs = document.querySelectorAll('.js-input');
 
-alertError.addEventListener('click', function () {
-    alert('Ошибка - это повод хорошо подумать:)')
-})
+    
+    document.querySelector('.arrow-left-btn').addEventListener('click', function() {
+        offset = offset + 750;
+        if (offset > 2000) {
+            offset = 0;
+        }
+        sliderLine.style.left = -offset + 'px';
+    })
 
-// выпадающий список
-document.addEventListener('DOMContentLoaded', hiddenCloseclick());
-document.getElementById('click-to-hide').addEventListener('click', hiddenCloseclick);
+    document.querySelector('.arrow-right-btn').addEventListener('click', function() {
+        offset = offset - 750;
+            if (offset < 0) {
+                offset = 1500;
+            }
+            sliderLine.style.left = -offset + 'px';
+    })
 
-function hiddenCloseclick() {
-	let x = document.getElementById('hidden-element');
-      if (x.style.display == "none"){
-	  x.style.display = "block";
-	  } else {
-	 x.style.display = "none"}
+    alertError.addEventListener('click', function () {
+        alert('Ошибка - это повод хорошо подумать:)')
+    })
+
+    // выпадающий список
+    const listBtnMenu = document.querySelectorAll('.sub-heading');
+    const list = document.querySelectorAll('.experience');
+
+    listBtnMenu[0].addEventListener('click', () => {
+        list[0].classList.toggle('block-none')
+    })
+    
+    listBtnMenu[1].addEventListener('click', () => {
+        list[1].classList.toggle('block-none')
+    })
+
+    function hiddenCloseclick() {
+        let x = document.getElementById('hidden-element');
+            if (x.style.display == "none"){
+                x.style.display = "block";
+            } else {
+                x.style.display = "none"}
     };
+
+
 
 
 
